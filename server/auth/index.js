92c132125d0ca7748750cb00bc60da8b2e7d56f8
boone84 <boone84@Boone-PC.localdomain>
2023-11-15 17:43:49 -0500

    user reg

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,13 +1,33 @@
 const router = require("express").Router();
+const bcrypt = require('bcryptjs');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-router.get("/", (req, res) =>{
-    res.send("auth router");
+router.get("/", (req, res) => {
+    res.send("Auth router");
 });
 
-// register a user
+// Register a user
+router.post("/register", async (req, res) => {
+    try {
+        const { password, ...userData } = req.body;
+        const saltRounds = 10; // Or use an environment variable for this
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-// router.post("/register");
+        const result = await prisma.user.create({
+            data: {
+                ...userData,
+                password: hashedPassword,
+            },
+        });
 
-
+        // Exclude the password from the response
+        const { password: _, ...userWithoutPassword } = result;
+        res.status(201).json(userWithoutPassword);
+    } catch (error) {
+        // You can refine this to handle specific types of errors more gracefully
+        res.status(500).json({ error: error.message });
+    }
+});
 
 module.exports = router;
